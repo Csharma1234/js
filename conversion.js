1e9c323d445fc2e1952c623edd9a56bcc5da9399
CHAHAT SHARMA <113041513+Csharma1234@users.noreply.github.com>
2024-11-03 14:03:18 +0000

    conversio  of js

---
+++ conversion.js
@@ -0,0 +1,12 @@
+let score = "3abs0"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
